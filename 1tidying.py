58c9a5dd69f708a895809724dcb58f40ee962a69
fuyong7335 <yukie353yukie@gmail.com>
2025-11-02 09:17:06 +0900

    Add files via upload

--- 1tidying.py
+++ 1tidying.py
@@ -137,5 +137,6 @@ if submitted:
 
     st.markdown("## 🔍 診断結果")
     st.markdown(f"あなたは **{type_label}** かもしれません。")
-    st.markdown("あなたのタイプに合ったアドバイスを受け取るには、以下をクリックしてください：")
-    st.markdown(f"[📩 アドバイスを受けとる]({link_url})")
+    st.write("あなたのタイプに合ったアドバイスを受け取りたい方は、以下のボタンをクリックしてください👇")
+    st.markdown(f"[📩 アドバイスを受け取る（無料）]({link_url})")
+
